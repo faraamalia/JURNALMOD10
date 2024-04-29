6cc0abfa04b0e162a6e0565b079c63cac65fcdde
unknown <frarsa18@gmail.com>
2024-04-29 07:40:13 +0700

    menambahkan API Mahasiswa

---
+++ modul10_1302223137/Controllers/MahasiswaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace modul10_1302223137.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        { 
+            new Mahasiswa ("Farah Amalia Putri", "1302223137",new List<string>{"KPL","PBO","BD"}, 2022),
+            new Mahasiswa ("Faris Azhar Dwiputra", "1302223123", new List<string>{"KPL","PBO","BD"}, 2022),
+            new Mahasiswa ("Wildan Syukri Ni'am", "1302220005", new List<string>{"KPL","PBO","BD"}, 2022),
+            new Mahasiswa ("Muhammad Bintang Al-Fath", "1302223099", new List<string>{"KPL","PBO","BD"}, 2022),
+            new Mahasiswa ("Arga Adolf Lumunon", "1302223038", new List<string>{"KPL","PBO","BD"}, 2022),
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswaList;
+        }
+
+        //GET api/<MahasiswaController>/5
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mahasiswaList[id];
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mahasiswaList.RemoveAt(id);
+        }
+
+        //POST api/<MahasiswaController>
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            mahasiswaList.Add(value);
+        }
+    }
+
+}
+
---
+++ modul10_1302223137/Mahasiswa.cs
@@ -0,0 +1,19 @@
+namespace modul10_1302223137.Controllers
+{
+
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public List<string> Course { get; set; }
+        public int Year { get; set; }
+
+        public Mahasiswa(string Nama, string Nim, List<string> Course, int Year)
+        {
+            this.Nama = Nama;
+            this.Nim = Nim;
+            this.Course = Course;   
+            this.Year = Year;
+        }
+    }
+}
